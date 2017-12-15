0a37aa8275469ae6e84924dc4a167826d1c67ff7
Daniel Nunes <danielnunesgnu@gmail.com>
2017-12-15 20:57:58 -0300

    Usando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+// Importações para funcionamento do Firebase e da Autenticação
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// Para usar o serviço de banco de dados é necessário importar o AngularFirestoreModule
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +18,21 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+
+
+// Importação da página de login
+import { LoginPage } from '../pages/login/login';
+import { TarefasProvider } from '../providers/tarefas/tarefas';
+
+
+// Adicionado páginas de Adicionar tarefa e  listar tarefas concluídas
+import { AdicionarTarefaPage } from '../pages/adicionar-tarefa/adicionar-tarefa';
+import { TarefasFinalizadasPage } from '../pages/tarefas-finalizadas/tarefas-finalizadas';
+
+
+// Configurações do FIREBASE
+import { config } from '../config';
 
 @NgModule({
   declarations: [
@@ -17,11 +40,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage, // Registrando a página de login
+    AdicionarTarefaPage, // Registrando a página de adicionar tarefa
+    TarefasFinalizadasPage, // Registrando a página de tarefas finalizadas
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+  
+    // Configurações do Firebase
+    AngularFireModule.initializeApp(config),
+    // Configuração do serviço de autenticação do firebase
+    AngularFireAuthModule,
+    // Configuração do serviço de banco de dados do firebase
+    AngularFirestoreModule,
+  
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +63,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage, // Registrando a página de login
+    AdicionarTarefaPage, // Registrando a página de adicionar tarefa
+    TarefasFinalizadasPage, // Registrando a página de tarefas finalizadas
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    TarefasProvider
   ]
 })
 export class AppModule {}
